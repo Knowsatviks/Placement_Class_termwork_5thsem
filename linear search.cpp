4dbae205a0f5024f865ce3327f50ce02e2c721bd
Knowsatviks <95932478+Knowsatviks@users.noreply.github.com>
2023-01-05 18:03:39 +0530

    Add files via upload

---
+++ linear search.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[5]={1,2,3,4,5};
+int key;
+cout<<"Enter the element to be found\n";
+cin>>key;
+for(int i=0;i<5;i++)
+{
+    if(arr[i]==key)
+    cout<<"element found";
+
+    else
+    cout<<"Element not found";
+}
+return 0;
+}
