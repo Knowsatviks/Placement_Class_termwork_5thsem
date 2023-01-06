ede3141957dd7d5bbc359027c1d138c529ccd63e
Knowsatviks <95932478+Knowsatviks@users.noreply.github.com>
2023-01-06 21:34:25 +0530

    Add files via upload

---
+++ fibbonacci_iterative.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void fib_iterative(int n){
+     int a=0,b=1,c;
+     cout<<a<<" "<<b;
+     for(int i=0;i<n;i++)
+     {
+         c=a+b;
+         cout<<" "<<c;
+         a=b;
+         b=c;
+     }
+}
+
+int main(){
+    int num;
+   cout<<"Enter the size of series\n";
+   cin>>num;
+   fib_iterative(num);
+   return 0;
+}
