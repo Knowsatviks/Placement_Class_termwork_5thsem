3a4daca23c7c7fd976a553e0aa517eff31340544
Knowsatviks <95932478+Knowsatviks@users.noreply.github.com>
2023-01-10 21:35:46 +0530

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size) {
+
+
+  for (int j = 0; j < size - 1; ++j) {
+    for (int i = 0; i < size - j - 1; ++i) {
+
+      if (arr[i] > arr[i + 1]) {
+
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
+    }
+  }
+}
+
+void display(int arr[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", arr[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int arr[] = {-2, 45, 0, 11, -9};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  bubbleSort(arr, size);
+  printf("Sorted array:\n");
+  display(arr, size);
+}
